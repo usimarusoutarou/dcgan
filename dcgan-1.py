374507f265e0c0c99182d4bc113a00b29bea9f63
nagalab <no email>
2019-07-30 15:11:55 +0900

    Generator layer change

--- dcgan-1.py
+++ dcgan-1.py
@@ -25,30 +25,87 @@ else:
 class DCGAN_Generator_NN(chainer.Chain):
 
 	def __init__(self):
-		# 重みデータの初期値を指定する
-		w = chainer.initializers.Normal(scale=0.02, dtype=None)
-		super(DCGAN_Generator_NN, self).__init__()
-		# 全ての層を定義する
-		with self.init_scope():
-			self.l0 = L.Linear(100, neuron_size * image_size * image_size // 8 // 8,
-							   initialW=w)
-			self.dc1 = L.Deconvolution2D(neuron_size, neuron_size // 2, 4, 2, 1, initialW=w)
-			self.dc2 = L.Deconvolution2D(neuron_size // 2, neuron_size // 4, 4, 2, 1, initialW=w)
-			self.dc3 = L.Deconvolution2D(neuron_size // 4, neuron_size // 8, 4, 2, 1, initialW=w)
-			self.dc4 = L.Deconvolution2D(neuron_size // 8, 3, 3, 1, 1, initialW=w)
-			self.bn0 = L.BatchNormalization(neuron_size * image_size * image_size // 8 // 8)
-			self.bn1 = L.BatchNormalization(neuron_size // 2)
-			self.bn2 = L.BatchNormalization(neuron_size // 4)
-			self.bn3 = L.BatchNormalization(neuron_size // 8)
+		super(DCGAN_Generator_NN, self).__init__(
+			c0=L.Convolution2D(4, 32, 3, 1, 1),
+            c1=L.Convolution2D(32, 64, 4, 2, 1),
+            c2=L.Convolution2D(64, 64, 3, 1, 1),
+            c3=L.Convolution2D(64, 128, 4, 2, 1),
+            c4=L.Convolution2D(128, 128, 3, 1, 1),
+            c5=L.Convolution2D(128, 256, 4, 2, 1),
+            c6=L.Convolution2D(256, 256, 3, 1, 1),
+            c7=L.Convolution2D(256, 512, 4, 2, 1),
+            c8=L.Convolution2D(512, 512, 3, 1, 1),
 
-	def __call__(self, z):
-		shape = (len(z), neuron_size, image_size // 8, image_size // 8)
-		h = F.reshape(F.relu(self.bn0(self.l0(z))), shape)
-		h = F.relu(self.bn1(self.dc1(h)))
-		h = F.relu(self.bn2(self.dc2(h)))
-		h = F.relu(self.bn3(self.dc3(h)))
-		x = F.sigmoid(self.dc4(h))
-		return x	# 結果を返すのみ
+            dc8=L.Deconvolution2D(1024, 512, 4, 2, 1),
+            dc7=L.Convolution2D(512, 256, 3, 1, 1),
+            dc6=L.Deconvolution2D(512, 256, 4, 2, 1),
+            dc5=L.Convolution2D(256, 128, 3, 1, 1),
+            dc4=L.Deconvolution2D(256, 128, 4, 2, 1),
+            dc3=L.Convolution2D(128, 64, 3, 1, 1),
+            dc2=L.Deconvolution2D(128, 64, 4, 2, 1),
+            dc1=L.Convolution2D(64, 32, 3, 1, 1),
+            dc0=L.Convolution2D(64, 3, 3, 1, 1),
+
+            bnc0=L.BatchNormalization(32),
+            bnc1=L.BatchNormalization(64),
+            bnc2=L.BatchNormalization(64),
+            bnc3=L.BatchNormalization(128),
+            bnc4=L.BatchNormalization(128),
+            bnc5=L.BatchNormalization(256),
+            bnc6=L.BatchNormalization(256),
+            bnc7=L.BatchNormalization(512),
+            bnc8=L.BatchNormalization(512),
+
+            bnd8=L.BatchNormalization(512),
+            bnd7=L.BatchNormalization(256),
+            bnd6=L.BatchNormalization(256),
+            bnd5=L.BatchNormalization(128),
+            bnd4=L.BatchNormalization(128),
+            bnd3=L.BatchNormalization(64),
+            bnd2=L.BatchNormalization(64),
+            bnd1=L.BatchNormalization(32)
+            # l = L.Linear(3*3*256, 2)'
+		)
+
+		def calc(self, x):
+        e0 = F.relu(self.bnc0(self.c0(x)))
+        e1 = F.relu(self.bnc1(self.c1(e0)))
+        e2 = F.relu(self.bnc2(self.c2(e1)))
+        del e1
+        e3 = F.relu(self.bnc3(self.c3(e2)))
+        e4 = F.relu(self.bnc4(self.c4(e3)))
+        del e3
+        e5 = F.relu(self.bnc5(self.c5(e4)))
+        e6 = F.relu(self.bnc6(self.c6(e5)))
+        del e5
+        e7 = F.relu(self.bnc7(self.c7(e6)))
+        e8 = F.relu(self.bnc8(self.c8(e7)))
+
+        d8 = F.relu(self.bnd8(self.dc8(F.concat([e7, e8]))))
+        del e7, e8
+        d7 = F.relu(self.bnd7(self.dc7(d8)))
+        del d8
+        d6 = F.relu(self.bnd6(self.dc6(F.concat([e6, d7]))))
+        del d7, e6
+        d5 = F.relu(self.bnd5(self.dc5(d6)))
+        del d6
+        d4 = F.relu(self.bnd4(self.dc4(F.concat([e4, d5]))))
+        del d5, e4
+        d3 = F.relu(self.bnd3(self.dc3(d4)))
+        del d4
+        d2 = F.relu(self.bnd2(self.dc2(F.concat([e2, d3]))))
+        del d3, e2
+        d1 = F.relu(self.bnd1(self.dc1(d2)))
+        del d2
+        d0 = self.dc0(F.concat([e0, d1]))
+
+        return d0
+
+	def __call__(self, x, t):
+        h = self.calc(x)
+        loss = F.mean_absolute_error(h, t)
+        chainer.report({'loss': loss}, self)
+        return loss
 
 # 画像を確認するNN
 class DCGAN_Discriminator_NN(chainer.Chain):
